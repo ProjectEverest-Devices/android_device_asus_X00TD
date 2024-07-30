6fe79ac927955b67789f843c04cdf7096c8164dd
dotkit <dotkit@electrowizard.me>
2024-07-30 16:21:43 +0000

    X00TD: `Everest-ify`
    Change-Id: If5b1cf62288ebdc29405958c473566a9c5014bad
    Signed-off-by: dotkit <dotkit@electrowizard.me>

--- everest_X00TD.mk
+++ everest_X00TD.mk
@@ -9,14 +9,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common EverestOS stuff
+$(call inherit-product, vendor/everest/config/common_full_phone.mk)
+
+# EverestOS official flags
+EVEREST_BUILD_TYPE := OFFICIAL
+EVEREST_MAINTAINER := ∂σткιт
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_BLUR := true
+
+# GApps flags
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LAWNCHAIR := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := false
+WITH_GAPPS := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := everest_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -30,9 +42,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
-    
+
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "asus/RU_X00TD/ASUS_X00T_6:10/QKQ1/17.2017.2012.438-20201203:user/release-keys"
+BUILD_FINGERPRINT := "asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys"
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.vendor.build.fingerprint=$(BUILD_FINGERPRINT) \
